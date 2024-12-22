f4039e654c75cc51d5e9d988f259ce70cfa340c3
Tabish Zaidi <tabishz@gmail.com>
2024-12-21 17:45:48 -0700

    moduling the js

--- eslint.config.js
+++ eslint.config.js
@@ -1,45 +1,36 @@
-import js from "@eslint/js";
-import react from "eslint-plugin-react";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import globals from "globals";
+import js from '@eslint/js';
+import react from 'eslint-plugin-react';
+import globals from 'globals';
 
 export default [
-  { ignores: ["dist"] },
+  { ignores: ['dist'] },
   {
-    files: ["**/*.{js,jsx}"],
+    files: ['**/*.{js,jsx}'],
     languageOptions: {
       ecmaVersion: 2024,
       globals: globals.browser,
       parserOptions: {
-        ecmaVersion: "latest",
+        ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
-        sourceType: "module",
+        sourceType: 'module',
       },
     },
-    settings: { react: { version: "18.3" } },
+    settings: { react: { version: '18.3' } },
     plugins: {
       react,
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
     },
     rules: {
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
-      ...react.configs["jsx-runtime"].rules,
-      ...reactHooks.configs.recommended.rules,
-      "react/jsx-no-target-blank": "off",
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
-      quotes: ["warn", "single"],
-      semi: ["error", "always"],
-      "no-unused-vars": "warn",
-      "no-unreachable": "error",
-      "no-unexpected-multiline": "error",
-      "max-depth": ["error", 6],
-      "no-multiple-empty-lines": ["error", { "max": 1 }],
+      ...react.configs['jsx-runtime'].rules,
+      'react/jsx-no-target-blank': 'off',
+      quotes: ['warn', 'single'],
+      semi: ['error', 'always'],
+      'no-unused-vars': 'warn',
+      'no-unreachable': 'error',
+      'no-unexpected-multiline': 'error',
+      'max-depth': ['error', 6],
+      'no-multiple-empty-lines': ['error', { max: 1 }],
     },
   },
 ];
